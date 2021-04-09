388b38d73a0bc6d75c29ef0f42f1fd59b8b150d8
Bradley Hong <bradleyhong37@gmail.com>
2021-04-09 14:55:09 -0500

    Implement the test for App.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Home component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const homeComponent = document.querySelector('#home');
+  expect(homeComponent).toBeInTheDocument();
 });
